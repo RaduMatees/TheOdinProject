98c93a2a12c13641a4d00d06f9c727219adc7bf1
Radu Matees <radumatees@gmail.com>
2017-08-22 22:07:17 +0300

    Sketchpad work in progress

---
+++ sketchpad/README.md
@@ -0,0 +1,1 @@
+Sketchpad Project using JQuery
--- sketchpad/scripts/script.js
+++ sketchpad/scripts/script.js
@@ -1,1 +1,19 @@
-alert('Hello')
+$(document).ready(function() {
+  takeUserInput();
+  makeGrid();
+});
+
+function takeUserInput() {
+  $('#button').click(function(){
+    var userInput = $('input[name=inputButton]').val();
+  return userInput;
+  });
+};
+
+function makeGrid() {
+  var gridWidth = $('.container').css('width');
+  var squareSize = gridWidth / takeUserInput();
+  var square = $("<div class='square'></div>");
+
+  $('.container').append($(square).css('width', '20px').css('height', '20px'));
+}
