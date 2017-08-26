57af6de6d85c7a078bd236098336536a08073d09
Radu Matees <radumatees@gmail.com>
2017-08-26 15:00:41 +0300

    create initial grid, prompt for user input and clear screen

--- sketchpad/scripts/script.js
+++ sketchpad/scripts/script.js
@@ -1,19 +1,35 @@
 $(document).ready(function() {
-  takeUserInput();
   makeGrid();
+  hoverEffect();
+  clearScreen();
+  takeUserInput();
 });
 
 function takeUserInput() {
-  $('#button').click(function(){
-    var userInput = $('input[name=inputButton]').val();
-  return userInput;
+  $('#button').on('click', function() {
+    var size = prompt('Enter a number of squares/row/column between 2 and 40:')
+    return size;
   });
 };
 
 function makeGrid() {
-  var gridWidth = $('.container').css('width');
-  var squareSize = gridWidth / takeUserInput();
-  var square = $("<div class='square'></div>");
 
-  $('.container').append($(square).css('width', '20px').css('height', '20px'));
+  for (var i = 0; i < 10; i++) {
+    $('.container').append("<div class='row'></div>"); // Each grid gets 16 rows
+  }
+  for (var i = 0; i < 10; i++) {
+    $('.row').append("<div class='square'></div>"); // Each row gets 16 squares
+  }
+}
+
+function hoverEffect() {
+  $('.square').on('mouseover', function() {
+    $(this).css('background-color', 'red');
+  })
+};
+
+function clearScreen() {
+  $('#clear-button').on('click', function() {
+    $('.square').css('background-color', 'white');
+  });
 }
