0cb3c9e39fa7332bc1eaf4d3f1196cad5a5b06f6
Radu Matees <radumatees@gmail.com>
2017-08-27 17:35:32 +0300

    Resolve grid resizing

--- sketchpad/scripts/script.js
+++ sketchpad/scripts/script.js
@@ -1,25 +1,38 @@
 $(document).ready(function() {
-  makeGrid();
-  hoverEffect();
-  clearScreen();
   takeUserInput();
+  makeGrid();
+  clearScreen();
 });
 
 function takeUserInput() {
   $('#button').on('click', function() {
-    var size = prompt('Enter a number of squares/row/column between 2 and 40:')
-    return size;
+    var gridWidth = parseInt($('.container').css('width'));
+    var size = prompt('Enter a number of squares/row/column between 2 and 40:');
+    var dimensions = gridWidth / size;
+
+    // remove previous grid
+    $('.row').remove();
+
+    // create custom grid
+    for (var i = 0; i < size; i++) {
+      $('.container').append($("<div class='row'></div>").css('height', dimensions));
+    }
+    for (var i = 0; i < size; i++) {
+      $('.row').append($("<div class='square'></div>").css('height', dimensions).css('width', dimensions));
+    }
+    hoverEffect();
   });
 };
 
 function makeGrid() {
 
   for (var i = 0; i < 10; i++) {
-    $('.container').append("<div class='row'></div>"); // Each grid gets 16 rows
+    $('.container').append("<div class='row'></div>") // Each grid gets 10 rows
   }
   for (var i = 0; i < 10; i++) {
-    $('.row').append("<div class='square'></div>"); // Each row gets 16 squares
+    $('.row').append("<div class='square'></div>"); // Each row gets 10 squares
   }
+  hoverEffect();
 }
 
 function hoverEffect() {
